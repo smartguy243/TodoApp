04af03fdc7e41ab857df4ee20ea8e05fbe8e7009
SmartGuy <kadiayieraste@mail.ru>
2025-04-24 09:39:25 +0300

    I came back to Room database's version 1

--- app/src/main/java/com/example/mytodoapp/data/local/TodoDatabase.kt
+++ app/src/main/java/com/example/mytodoapp/data/local/TodoDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.mytodoapp.data.local.dao.TodoDao
 import com.example.mytodoapp.data.local.entity.TodoItem
 
-@Database(entities = [TodoItem::class], version = 2, exportSchema = false)
+@Database(entities = [TodoItem::class], version = 1, exportSchema = false)
 abstract class TodoDatabase : RoomDatabase() {
 
     abstract fun todoDao(): TodoDao
